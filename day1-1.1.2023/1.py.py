9ce40d171ecb20d2ff5e0d3e6fa7cd1213a1e39a
Deeksha Jatangi D <79524527+DEEKSHACodeaim@users.noreply.github.com>
2023-01-02 19:52:55 +0530

    Delete day1-1.1.2023 directory

--- day1-1.1.2023/1.py.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        sum_i=0
-        sum_all=0
-        for i in range(len(nums)):
-            sum_i=nums[i]
-            nums[i]=sum_all+sum_i
-            sum_all=sum_all+sum_i
-        return nums
